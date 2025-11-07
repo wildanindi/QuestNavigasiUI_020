eb50a80f4591b4f9ddd0f1cd77815c41efe7c535
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-07 16:21:19 +0700

    Menambah Scaffold untuk header bar

--- app/src/main/java/com/example/questnavigasiui_020/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_020/view/TampilData.kt
@@ -1,9 +1,30 @@
 package com.example.questnavigasiui_020.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questnavigasiui_020.R
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 
@@ -15,6 +36,14 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta"),
-
     )
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+            )
+        }
+    ){
+    }
 }
