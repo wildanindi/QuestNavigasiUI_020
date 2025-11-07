5546774b54035399bd9829bce128f34e07a7967c
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-07 16:02:59 +0700

    Menambah function tampildata dan valueable untuk nama_lengkap, jenis_kelamin, dan alamat

--- app/src/main/java/com/example/questnavigasiui_020/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_020/view/TampilData.kt
@@ -1,2 +1,20 @@
 package com.example.questnavigasiui_020.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_020.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun TampilData(
+    onBackBtnClick:()-> Unit
+){
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta"),
+
+    )
+}
