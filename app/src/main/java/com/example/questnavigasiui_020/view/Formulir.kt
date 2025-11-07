e98c29fe28372cec8ef13f6e9410aaf9c74004db
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-07 15:30:29 +0700

    Menambah scaffold untuk membuat header pada tampilan ui

--- app/src/main/java/com/example/questnavigasiui_020/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_020/view/Formulir.kt
@@ -1,10 +1,27 @@
 package com.example.questnavigasiui_020.view
 
-
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
-
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.questnavigasiui_020.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 
@@ -13,5 +30,16 @@ fun FormIsian(
     jenisK: List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+                )}
 
+
+    ) {
+
+
+    }
 }
