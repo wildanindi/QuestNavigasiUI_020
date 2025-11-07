e3808ed72cb149f1d28fb46fe041eaa0ea0a9596
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-07 16:27:07 +0700

    Menambah spacer untuk jarak padding, dan button back untuk kembali

--- app/src/main/java/com/example/questnavigasiui_020/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_020/view/TampilData.kt
@@ -56,7 +56,12 @@ fun TampilData(
                     }
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
-
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick) {
+                    Text(text = stringResource(id = R.string.back))
+                }
             }
         }
 
