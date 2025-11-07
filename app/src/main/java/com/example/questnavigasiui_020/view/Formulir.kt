284a7b879e3b7c96001865c94c8390fb9e8b9d14
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-07 15:27:46 +0700

    Menambah file formulir dan tampildata, sekaligus menambah function untuk pemilihan jenis kelamin

---
+++ app/src/main/java/com/example/questnavigasiui_020/view/Formulir.kt
@@ -0,0 +1,17 @@
+package com.example.questnavigasiui_020.view
+
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+import androidx.compose.runtime.Composable
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+
+}
---
+++ app/src/main/java/com/example/questnavigasiui_020/view/TampilData.kt
@@ -0,0 +1,2 @@
+package com.example.questnavigasiui_020.view
+
